1633b879750f9dc8e50ed6aaf4911c8ad6f51191
Stephan-e <astephanerasmus@gmail.com>
2020-04-22 16:22:19 +0800

    gitignore

--- config/plugins/email.py
+++ config/plugins/email.py
@@ -1,6 +1,6 @@
 import os
 
-SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY', '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')
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 
 ANYMAIL = {
     "SENDGRID_API_KEY": SENDGRID_API_KEY,
